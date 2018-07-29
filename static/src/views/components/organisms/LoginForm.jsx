1dc9142693353c262407f84a49b2cf0bb32217bc
Janett Hickel <janett.hickel@googlemail.com>
2018-07-29 14:26:52 +0200

    [TASK]: Change to pure components

--- static/src/views/components/organisms/LoginForm.jsx
+++ static/src/views/components/organisms/LoginForm.jsx
@@ -1,6 +1,6 @@
 // @flow
 
-import React from 'react'
+import * as React from 'react'
 import styled from 'styled-components'
 
 import Button from 'views/components/atoms/Button'
@@ -11,13 +11,17 @@ const Wrapper = styled(Form)`
   margin-top: ${props => (props.theme.buildingUnit * 12)}px;
 `
 
-const LoginForm = () => (
-  <Wrapper>
-    <FormGroup id='username' type='text' label='Username' />
-    <FormGroup id='password' type='password' label='Password' />
-    <Button type='submit'>Test</Button>
-  </Wrapper>
-)
+class LoginForm extends React.PureComponent<Object> {
+  render () {
+    return (
+      <Wrapper>
+        <FormGroup id='username' type='text' label='Username' />
+        <FormGroup id='password' type='password' label='Password' />
+        <Button type='submit'>Login</Button>
+      </Wrapper>
+    )
+  }
+}
 
 export {
   LoginForm as default
--- static/src/views/pages/Home.jsx
+++ static/src/views/pages/Home.jsx
@@ -1,17 +1,21 @@
 // @flow
 
-import React from 'react'
+import * as React from 'react'
 import styled from 'styled-components'
 
 import H1 from 'views/components/atoms/H1'
 
 const Wrapper = styled.div``
 
-const Home = () => (
-  <Wrapper>
-    <H1>BGAchievements</H1>
-  </Wrapper>
-)
+class Home extends React.PureComponent<Object> {
+  render () {
+    return (
+      <Wrapper>
+        <H1>BGAchievements</H1>
+      </Wrapper>
+    )
+  }
+}
 
 export {
   Home as default
