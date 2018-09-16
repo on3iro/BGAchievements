aafb0e737eabca915ee018b379ce32b4f08ea2c1
Theo Salzmann <theo.salzmann@sandstorm.de>
2018-09-16 11:25:52 +0200

    [TASK]: Fix gitignore and add CORS to API

--- src/main/kotlin/org/oneironauten/bgachievements/controllers/HelloController.kt
+++ src/main/kotlin/org/oneironauten/bgachievements/controllers/HelloController.kt
@@ -3,6 +3,7 @@ package org.oneironauten.bgachievements.controllers
 import org.oneironauten.bgachievements.dbo.Hello
 import org.oneironauten.bgachievements.repositories.HelloRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,6 +13,7 @@ class HelloController {
     @Autowired
     lateinit var helloRepository: HelloRepository
 
+    @CrossOrigin()
     @RequestMapping("/hello")
     fun messages(): MutableList<Hello> = helloRepository.findAll()
 
